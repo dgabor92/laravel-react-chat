8ea60426aebf9d01061992e229e42d73c98605d2
Gábor <deutschgabor92@gmail.com>
2023-11-27 14:11:40 +0100

    welcome, login, register page update

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -32,7 +32,10 @@ export default function Navbar({ user }: NavbarProps) {
   });
 
   return (
-    <Disclosure as="nav" className="bg-purple-800">
+    <Disclosure
+      as="nav"
+      className="bg-gradient-to-r from-blue-600 to-purple-400"
+    >
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -50,31 +53,11 @@ export default function Navbar({ user }: NavbarProps) {
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        onClick={() => navigate(item.href)}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white cursor-pointer",
-                          "rounded-md px-3 py-2 text-sm font-medium cursor-pointer"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
+                <div
+                  onClick={() => navigate("/dashboard")}
+                  className="flex flex-shrink-0 text-white uppercase items-center hover:cursor-pointer hover:text-gray-300 text-xl font-semibold"
+                >
+                  Laravel-React-Chat
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -27,16 +27,16 @@ const Signup = () => {
   };
 
   return (
-    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
-        <h1 className="text-3xl font-semibold text-center text-purple-700 uppercase">
+    <div className="relative flex flex-col justify-center bg-gradient-to-r from-blue-600 to-purple-400 min-h-screen overflow-hidden">
+      <div className="w-full p-6 m-auto rounded-md lg:max-w-xl">
+        <h1 className="text-3xl font-semibold text-center text-white uppercase">
           Sign up
         </h1>
         <form className="mt-6" onSubmit={handleSubmit}>
           <div className="mb-2">
             <label
               htmlFor="name"
-              className="block text-sm font-semibold text-gray-800"
+              className="block text-sm font-semibold text-white"
             >
               Name
             </label>
@@ -45,14 +45,14 @@ const Signup = () => {
               id="name"
               value={form.name}
               onChange={(e) => setForm({ ...form, name: e.target.value })}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-gradient-to-r from-white to-gray-400 border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
               required
             />
           </div>
           <div className="mb-2">
             <label
               htmlFor="email"
-              className="block text-sm font-semibold text-gray-800"
+              className="block text-sm font-semibold text-white"
             >
               Email
             </label>
@@ -61,14 +61,14 @@ const Signup = () => {
               id="email"
               value={form.email}
               onChange={(e) => setForm({ ...form, email: e.target.value })}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-gradient-to-r from-white to-gray-400 border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
               required
             />
           </div>
           <div className="mb-2">
             <label
               htmlFor="password"
-              className="block text-sm font-semibold text-gray-800"
+              className="block text-sm font-semibold text-white"
             >
               Password
             </label>
@@ -77,14 +77,14 @@ const Signup = () => {
               id="password"
               value={form.password}
               onChange={(e) => setForm({ ...form, password: e.target.value })}
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-gradient-to-r from-white to-gray-400 border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
               required
             />
           </div>
           <div className="mb-2">
             <label
               htmlFor="password"
-              className="block text-sm font-semibold text-gray-800"
+              className="block text-sm font-semibold text-white"
             >
               Password Confirmation
             </label>
@@ -95,21 +95,21 @@ const Signup = () => {
               onChange={(e) =>
                 setForm({ ...form, password_confirmation: e.target.value })
               }
-              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full px-4 py-2 mt-2 text-purple-700 bg-gradient-to-r from-white to-gray-400 border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
               required
             />
           </div>
           <div className="mt-6">
             <button
               type="submit"
-              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-purple-600"
             >
               Sign up
             </button>
           </div>
-          <p className="mt-4">
+          <p className="mt-4 text-xs text-white">
             Already have an account?{" "}
-            <Link to="/login" className="text-blue-500 hover:text-blue-600">
+            <Link to="/login" className="text-blue-700 hover:text-blue-600">
               Log in
             </Link>
           </p>
